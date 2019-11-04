779074a285c7d6b71ac239a926ae1313da5bcb38
Hurntre <adefolajuariyo@gmail.com>
2019-11-04 12:03:17 +0100

    chore: debug

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { AlbumStorePage } from '../../e2e/app.po';
-import { map } from 'rxjs/add/operator/map';
+import 'rxjs/add/operator/map';
 import { Album } from './album'
 import { Observable } from 'rxjs/Observable';
 import { Product } from './product'
